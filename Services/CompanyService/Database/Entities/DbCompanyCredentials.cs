7a2da3e725c40b74580253907d7b29d0b68c7146
asyagraf <67205781+asyagraf@users.noreply.github.com>
2020-10-09 01:47:20 +0300

    Database Entities added
    DbCompany.cs added
    DbCompanyCredentials.cs added

---
+++ Services/CompanyService/Database/Entities/DbCompanyCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studfolio.CompanyService.Database.Entities
+{
+    public class DbCompanyCredentials
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }        
+    }
+}
