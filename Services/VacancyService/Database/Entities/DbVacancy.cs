3642315fd3b4deec7b692cd955d85b0631be4d34
asyagraf <67205781+asyagraf@users.noreply.github.com>
2020-10-09 01:52:14 +0300

    Database Entities added
    DbVacancy.cs added

---
+++ Services/VacancyService/Database/Entities/DbVacancy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studfolio.VacancyService.Database.Entities
+{
+    public class DbVacancy
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public string Position { get; set; }
+        public ICollection<string> Tags { get; set; }
+        public string Description { get; set; }
+        public Guid CompanyId { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public int SalaryFrom {get; set; }
+        public int SalaryTo{ get; set; }
+        public string Currency { get; set; }
+        public bool Remote { get; set; }
+        public string Location { get; set; }
+        public bool FullTime { get; set; }
+    }
+}
