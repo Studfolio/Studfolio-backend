37441a1e6225ae0e3e4e6d3094f38fd80879ed1b
asyagraf <67205781+asyagraf@users.noreply.github.com>
2020-10-09 01:50:34 +0300

    Database Entities added
    DbEmail.cs added
    DbEmailTemplate.cs added

---
+++ Services/MessageService/Database/Entities/DbEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studfolio.MessageService.Database.Entities
+{
+    public class DbEmail
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        public Guid SenderId { get; set; }
+        [Required]
+        public string Receiver { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Services/MessageService/Database/Entities/DbEmailTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studfolio.MessageService.Database.Entities
+{
+    public class DbEmailTemplate
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
