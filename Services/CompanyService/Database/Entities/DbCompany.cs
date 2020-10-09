f3003e4338e98a7be911c400f1e0976b1134b69a
asyagraf <67205781+asyagraf@users.noreply.github.com>
2020-10-09 12:06:24 +0300

    Database Entities added (#3)
    DbCompany.cs added
    DbCompanyCredentials.cs added

---
+++ Services/CompanyService/Database/Entities/DbCompany.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Studfolio.CompanyService.Database.Entities
+{
+    public class DbCompany
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        public ICollection<Guid> HRIds { get; set; }
+        public ICollection<Guid> VacanciesIds { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
