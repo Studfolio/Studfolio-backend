b2cfb1fc0414e8032386760ab4cf8af0162aad2e
asyagraf <67205781+asyagraf@users.noreply.github.com>
2020-10-11 20:43:13 +0300

    MessageServise Migrations (#6)

---
+++ Services/MessageService/Database/MessageServiceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studfolio.MessageService.Database.Entities;
+using System.Reflection;
+
+namespace Studfolio.MessageService.Database
+{
+    class MessageServiceDbContext : DbContext
+    {
+        public MessageServiceDbContext(DbContextOptions<MessageServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DbEmail> Emails { get; set; }
+        public DbSet<DbEmailTemplate> EmailTemplates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Services/MessageService/Migrations/20201011164356_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Studfolio.MessageService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Subject = table.Column<string>(nullable: false),
+                    Body = table.Column<string>(nullable: false),
+                    SenderId = table.Column<Guid>(nullable: false),
+                    Receiver = table.Column<string>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Subject = table.Column<string>(nullable: false),
+                    Body = table.Column<string>(nullable: false),
+                    AuthorId = table.Column<Guid>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailTemplates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "EmailTemplates");
+        }
+    }
+}
