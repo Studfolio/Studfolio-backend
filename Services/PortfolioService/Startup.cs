61d64b3ce9083d256920c000b08daa11d0bf0521
amadeus-artis <semyonmoroshkin@gmail.com>
2020-10-13 01:24:52 +0300

    Revert "Second commit"
    This reverts commit cc6da543efc0e446753255061b0e676b1028fa3d.

--- Services/PortfolioService/Startup.cs
+++ Services/PortfolioService/Startup.cs
@@ -1,33 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Studfolio.PortfolioService.Database;
 
 namespace Studfolio.PortfolioService
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PortfolioServiceDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("SQLConnectionString"));
-            });
-
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,24 +22,15 @@ namespace Studfolio.PortfolioService
                 app.UseDeveloperExceptionPage();
             }
 
-            UpdateDatabase(app);
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
-        }
-
-        private void UpdateDatabase(IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices
-                .GetRequiredService<IServiceScopeFactory>()
-                .CreateScope();
-            using var context = serviceScope.ServiceProvider.GetService<PortfolioServiceDbContext>();
-
-            context.Database.Migrate();
         }
     }
 }
