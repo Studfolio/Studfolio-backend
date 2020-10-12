f62d9425c63c4bc59b41b0285fae0b196299fd3c
amadeus-artis <semyonmoroshkin@gmail.com>
2020-10-13 02:35:59 +0300

    PortfolioService first commit

---
+++ Services/PortfolioService/Database/PortfolioServiceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studfolio.PortfolioService.Database.Entities;
+
+namespace Studfolio.PortfolioService.Database
+{
+    class PortfolioServiceDbContext : DbContext
+    {
+        public PortfolioServiceDbContext(DbContextOptions<PortfolioServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DbPortfolio> Portfolios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
