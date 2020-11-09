e54ead4bcfa43b4a289f1e836c622025a3ea8499
Lena <59228285+lalagvana@users.noreply.github.com>
2020-11-09 15:59:35 +0300

    Update Startup.cs

--- Services/FileService/Startup.cs
+++ Services/FileService/Startup.cs
@@ -6,6 +6,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Studfolio.FileService.Database;
+using MassTransit;
+using Studfolio.FileService.Broker.Consumers;
+using Studfolio.FileService.Business.Interfaces;
+using Studfolio.FileService.Business;
+using Studfolio.FileService.Data.Interfaces;
+using Studfolio.FileService.Data;
+using Studfolio.FileService.Mappers.Interfaces;
+using Studfolio.FileService.Database.Entities;
+using Studfolio.FileService.Models.Dto;
+using Studfolio.FileService.Mappers;
+using FluentValidation;
+using Studfolio.FileService.Validation;
+using LT.DigitalOffice.Kernel;
 
 namespace Studfolio.FileService
 {
@@ -17,30 +30,96 @@ namespace Studfolio.FileService
         {
             Configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
+
             services.AddDbContext<FileServiceDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SQLConnectionString"));
             });
-
             services.AddControllers();
+
+            ConfigureCommands(services);
+            ConfigureMappers(services);
+            ConfigureRepositories(services);
+            ConfigureValidators(services);
+            ConfigureMassTransit(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        private void ConfigureMassTransit(IServiceCollection services)
         {
-            if (env.IsDevelopment())
+            const string serviceSection = "RabbitMQ";
+            string serviceName = Configuration.GetSection(serviceSection)["Username"];
+            string servicePassword = Configuration.GetSection(serviceSection)["Password"];
+
+            services.AddMassTransit(x =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                x.AddConsumer<GetFileConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("localhost", "/", host =>
+                    {
+                        host.Username($"{serviceName}_{servicePassword}");
+                        host.Password(servicePassword);
+                    });
+
+                    cfg.ReceiveEndpoint(serviceName, ep =>
+                    {
+                        ep.ConfigureConsumer<GetFileConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+        }
+
+        private void ConfigureCommands(IServiceCollection services)
+        {
+            services.AddTransient<IAddNewFileCommand, AddNewFileCommand>();
+            services.AddTransient<IGetFileByIdCommand, GetFileByIdCommand>();
+            services.AddTransient<IDisableFileByIdCommand, DisableFileByIdCommand>();
+        }
+
+        private void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IFileRepository, FileRepository>();
+        }
+
+        private void ConfigureMappers(IServiceCollection services)
+        {
+            services.AddTransient<IMapper<DbFile, File>, FileMapper>();
+            services.AddTransient<IMapper<FileCreateRequest, DbFile>, FileMapper>();
+        }
+
+        private void ConfigureValidators(IServiceCollection services)
+        {
+            services.AddTransient<IValidator<FileCreateRequest>, AddNewFileValidator>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/api/healthcheck");
+
+            app.UseExceptionHandler(tempApp => tempApp.Run(CustomExceptionHandler.HandleCustomException));
 
             UpdateDatabase(app);
 
+#if RELEASE
+            app.UseHttpsRedirection();
+#endif
+
             app.UseRouting();
+
+            string corsUrl = Configuration.GetSection("Settings")["CorsUrl"];
+
+            app.UseCors(builder =>
+                builder
+                    .WithOrigins(corsUrl)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
 
             app.UseEndpoints(endpoints =>
             {
@@ -54,7 +133,6 @@ namespace Studfolio.FileService
                 .GetRequiredService<IServiceScopeFactory>()
                 .CreateScope();
             using var context = serviceScope.ServiceProvider.GetService<FileServiceDbContext>();
-
             context.Database.Migrate();
         }
     }
